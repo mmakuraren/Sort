5f800891e323363dd95cc30f368919e9639db7f7
Matthew McLaren <mmclaren@purdue.edu>
2021-06-19 21:56:03 -0400

    Update Initialize.java
    changed some typos and began to write the format for the project

--- src/Initialize.java
+++ src/Initialize.java
@@ -17,7 +17,7 @@ public class Initialize {
 
     private static final String CHECKFILEPATH = "prog/check.txt";  // path for the check file
     private static final String RANDOMFILEPATH = "prog/random.txt";  // path for the random file
-    private static final String CASEFILEPATH = "prog/case.txt";  // path for the random file
+    private static final String CASEFILEPATH = "prog/case.txt";  // path for the case file
 
     /**
      * This static method creates the check.txt file in case something happens with it that shouldn't happen.
@@ -27,9 +27,29 @@ public class Initialize {
     public static void createCheckFile() throws InvalidPathException {
         // finds the file so it can override it
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(CHECKFILEPATH))) {
-            // adds the numbers from 1-1000
+            // adds the numbers from 1-10000000
             for (int i = 1; i <= 100000; i++) {
                 bw.write(i + "\n"); // adds the number in
+            }
+            bw.close();
+        } catch (IOException e) {
+            // this should only print if the path is bad
+            throw new InvalidPathException();
+        }
+    }
+
+    /**
+     * This is a static method that builds the random.txt file for sorting. Mostly will be used to initiate the tests.
+     *
+     * @param shuffleArray the shuffled array of numbers
+     * @throws InvalidPathException if invalid path is given
+     */
+    public static void createRandomFile(ArrayList<Integer> shuffleArray) throws InvalidPathException {
+        // finds the file so it can override it
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(RANDOMFILEPATH))) {
+            // adds the numbers from shuffled array list
+            for (Integer i : shuffleArray) {
+                bw.write(i + "\n");
             }
             bw.close();
         } catch (IOException e) {
@@ -60,17 +80,60 @@ public class Initialize {
         return checkArray;
     }
 
-    public static void createRandomFile(ArrayList<Integer> shuffleArray) throws InvalidPathException {
-        // finds the file so it can override it
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(RANDOMFILEPATH))) {
-            // adds the numbers from shuffled array list
-            for (Integer i : shuffleArray) {
-                bw.write(Integer.toString(i));
+    /**
+     * This static method returns the random array needed to keep track of progress.
+     *
+     * @return the random array inside of the random.txt file
+     */
+    public static ArrayList<Integer> parseRandomArray() {
+        ArrayList<Integer> randomArray = new ArrayList<>(); // initialize array list
+
+        // grabs values
+        try (BufferedReader br = new BufferedReader(new FileReader(RANDOMFILEPATH))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                randomArray.add(Integer.parseInt(line));
             }
-            bw.close();
         } catch (IOException e) {
-            // this should only print if the path is bad
-            throw new InvalidPathException();
+            e.printStackTrace();
+        }
+
+        // returns array list
+        return randomArray;
+    }
+
+    /**
+     * This static method will return a boolean for the result of how randomized the random.txt values are.
+     *
+     * @param array an array of numbers (starting at 1 to a limit)
+     * @return boolean expression responding to how shuffled the array is
+     */
+    public static boolean compareShuffle(ArrayList<Integer> array) {
+        // checks to make sure that the array is completely shuffled to start for max test results
+        int i = 1;
+        for (Integer number : array) {
+            if (number == i) {
+                return false;
+            } else {
+                i++;
+            }
+        }
+
+        return true; // returns if the array is shuffled
+    }
+
+    /**
+     * This static method will shuffle the numbers until everything is in a different spot from the original.
+     *
+     * @param array an array of numbers (starting at 1 to a limit)
+     */
+    public static void shuffleArray(ArrayList<Integer> array) {
+        Collections.shuffle(array); // shuffles the array
+
+        // checks to make sure that everything is shuffled
+        while (!compareShuffle(array)) {
+            Collections.shuffle(array); // shuffles the array again
+            System.out.println("shuffled again");
         }
     }
 
